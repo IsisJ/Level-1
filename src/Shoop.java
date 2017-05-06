a472fc123b657e3f15bf9b35f48e5356fcfabc62
IsisJ <league@WTS-MM-7.local>
2017-05-06 12:17:36 -0700

    5-6-17

--- src/Shoop.java
+++ src/Shoop.java
@@ -2,24 +2,46 @@
 public class Shoop {
 	private int tailLength;
 	private String shape;
+	private String clothes;
+	private String color;
+	private String colorWool;
 
 	public Shoop(int tailLength, String shape) {
 		this.tailLength = tailLength;
 		this.shape = shape;
 	}
 
-	public void lengthenTail() {
-		tailLength = tailLength + 2;
-		System.out.println(tailLength);
+	public void dressMe(String clothes, String color) {
+		this.clothes = clothes;
+		this.color = color;
+		System.out.println("I now have a " + color + " " + clothes + ".");
 	}
 
-	public void discribeYourSelf() {
-		System.out.println("I have the tail length of "+tailLength+" and a shape of a "+shape+".");
+	public void dyeWool(String colorWool) {
+		this.colorWool=colorWool;
+		System.out.println("Yay! I have "+colorWool+" wool!!");
+	}
+
+	public void lengthenTail() {
+		tailLength = tailLength + 2;
+		System.out.println("My tail is now "+tailLength+" feet long.");
+	}
+
+	public void describeYourSelf() {
+		System.out.println("I have the tail length of " + tailLength + " and a shape of a " + shape + ". I'm wearing a "+color+" colored "+clothes+" and I have "+colorWool+" wool.");
 	}
 
 	public static void main(String[] args) {
-		Shoop petCarl = new Shoop(3, "triangle");
-		petCarl.lengthenTail();
-		petCarl.discribeYourSelf();
+		Shoop isis= new Shoop(3,"Isis");
+		
+		isis.dressMe("shirt", "red");
+		isis.dyeWool("rose pink");
+		isis.describeYourSelf();
+		isis.dressMe("ripped jacket with ripped jeans", "black");
+		isis.describeYourSelf();
+		isis.dressMe("bomber jacket", "navy blue");
+		isis.lengthenTail();
+		isis.describeYourSelf();
+		
 	}
 }
