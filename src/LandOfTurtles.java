5f56159ce4024de7e8c33461f129282fbf7a1e40
IsisJ <league@Mac-mini-4.lan>
2017-06-03 12:20:44 -0700

    6-3-17

--- src/LandOfTurtles.java
+++ src/LandOfTurtles.java
@@ -1,10 +1,15 @@
 
+import java.util.Random;
+
 import javax.swing.JFrame;
 import org.teachingextensions.logo.MultiTurtlePanel;
 import org.teachingextensions.logo.Turtle;
 
 public class LandOfTurtles {
+
 	public static void main(String[] args) {
+		Random random = new Random();
+		Random randoms= new Random();
 		MultiTurtlePanel panel = new MultiTurtlePanel();
 		String galapagosIslands = "https://farm2.staticflickr.com/1104/752631367_5c5d474ba5_o.jpg";
 
@@ -17,16 +22,23 @@ public class LandOfTurtles {
 		panel.setBackgroundImage(galapagosIslands);
 		frame.pack();
 		// 4. Instantiate a Turtle
-		Turtle turtle= new Turtle();
+		Turtle turtle = new Turtle();
 		// 5. Add the turtle to the panel
-		int panelWidth=panel.getWidth();
-		int panelHeight=panel.getHeight();
-		for(int i= 0; i<50; i++){
-		panel.addTurtle(turtle);
-		//turtle.setX(x);
-		//turtle.setY(y);
+		int panelWidth = panel.getWidth();
+		int panelHeight = panel.getHeight();
+		System.out.println(panelHeight);
+		System.out.println(panelWidth);
+		for (int i = 0; i < 50; i++) {
+			int randomNum=random.nextInt(301);
+			int randomNums=randoms.nextInt(443);
+			Turtle newTurtle= new Turtle();
+			panel.addTurtle(newTurtle);
+			newTurtle.setX(randomNum);
+			newTurtle.setY(randomNums);
+			//System.out.println(randomNum);
+			//System.out.println(randomNums);
 		}
 		// 6. Put 50 Turtles on the beach
-		
+
 	}
 }
