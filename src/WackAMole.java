d340c11d94fc4df1852a7442c6e2d791925eb6c3
IsisJ <league@Mac-mini.local>
2017-10-07 12:18:38 -0700

    10-7-10

--- src/WackAMole.java
+++ src/WackAMole.java
@@ -1,4 +1,31 @@
 
 public class WackAMole {
-
+	/**create UI
+	 * 	frame
+	 * 	panel	
+	 * 	generate a random number from 1-20
+	 * 	for i= 1....20
+	 * 		makeButton()
+	 * 		if(i==randomnumber)
+	 * 			make text says "mole"
+	 *
+	 */
+	
+	/*action perform
+	 * 	check pressed button
+	 * 	if mole button pressed
+	 * 		disposed fram : frame.dispose();
+	 * 		createUI()
+	 * 	else
+	 * 		play loser sound effect
+	 * 
+	 */
+	
+	/**makeButton()
+	 * 	instantiate button
+	 * 	add listener
+	 * 	add to panel
+	 * 	
+	 * 	
+	 */
 }
