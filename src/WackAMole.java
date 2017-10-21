e2f433195f08ca54ea51505216fbf84461639b5a
IsisJ <league@FunkyMini.local>
2017-10-21 12:16:58 -0700

    10-21-17

--- src/WackAMole.java
+++ src/WackAMole.java
@@ -1,36 +1,31 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WackAMole implements ActionListener {
-	/**
-	 * create UI 
-	 * 	frame panel 
-	 * 	generate a random number from 1-20 
-	 * 	for i= 1....20 makeButton() 
-	 * 		if(i==randomnumber) make
-	 * 			text says "mole"
-	 *
-	 */
-	
 
+	Date gameStart = new Date();
+	Random random = new Random();
 	JFrame frame = new JFrame();
-	
+	int moleTracker;
+
 	public void createUI() {
-		
+		frame = new JFrame();
+
 		JPanel panel = new JPanel();
-		
+
 		frame.setVisible(true);
 		frame.setSize(400, 250);
 		frame.add(panel);
-		Random random = new Random();
-		int randomNum = random.nextInt(20);
-		System.out.println("creating new random number "+ randomNum+ ".");
+		int randomNum = random.nextInt(20) + 1;
+		System.out.println("creating new random number " + randomNum + ".");
 		for (int i = 1; i <= 25; i++) {
 			JButton button = new JButton();
 			button.addActionListener(this);
@@ -39,50 +34,48 @@ public class WackAMole implements ActionListener {
 				button.setText("Mole");
 			}
 		}
+
 	}
 
-	/*
-	 * action perform 
-	 * 	check pressed button 
-	 * 	if mole button pressed 
-	 * 		disposed frame : frame.dispose(); 
-	 * 		createUI() 
-	 * 	else play
-	 * 		loser sound effect
-	 * 
-	 */
+	public void gameTime() {
 
-	public void actionPerformed(ActionEvent e){
-		
-		JButton buttonClicked = (JButton)(e.getSource());
+		Date gameEnd = new Date();
+
+		long start = gameStart.getTime();
+		System.out.println("game start: " + gameStart);
+		long end = gameEnd.getTime();
+		System.out.println("game end: " + gameEnd);
+		long gameTime = end - start;
+		//long gameTimeInSec = gameTime/1000.0;
+		JOptionPane.showMessageDialog(null, "You wacked 10 moles in " + gameTime/1000.0 + ".");
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		JButton buttonClicked = (JButton) (e.getSource());
 		String isMole = buttonClicked.getText();
-		System.out.println(" text is "+ isMole+" .");
-		if(isMole.equals("Mole")){
+		System.out.println(" text is " + isMole + " .");
+		if (isMole.equals("Mole")) {
+			moleTracker = moleTracker + 1;
+			System.out.println(moleTracker + " mole(s)");
 			System.out.println("click");
-			frame.dispose();
-			this.createUI();
+			if (moleTracker < 10) {
+				frame.dispose();
+				this.createUI();
+			} else {
+				this.gameTime();
+			}
 		}
-		
-		
+
 	}
-	
-	
-	/**
-	 * makeButton()
-	 *  instantiate button 
-	 *  add listener
-	 *   add to panel
-	 * 
-	 * 
-	 */
 
 	public static void main(String[] args) {
 
 		WackAMole mole = new WackAMole();
 		mole.createUI();
 
-	}
 
-	
+	}
 
 }
